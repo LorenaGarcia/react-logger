41bad1f508a3b64a4141479073904595175fda42
LorenaGarcia <lorraine@Lorenas-MacBook-Pro.local>
2019-10-03 00:04:21 -0500

    delete

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -29,14 +29,19 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
 };
 
 Logs.propTypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
   log: state.log
 });
 
+const mapDispatchToProps = {
+  getLogs
+}
+
 export default connect(
   mapStateToProps,
-  { getLogs }
+  mapDispatchToProps
 )(Logs);
