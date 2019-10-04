185f2339cd2589bc30fcbd3011c41f12f7b516d5
LorenaGarcia <lorraine@Lorenas-MacBook-Pro.local>
2019-10-03 23:29:27 -0500

    EditLogModal

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,14 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from "../actions/types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  UPDATE_LOG,
+  SEARCH_LOGS,
+  SET_CURRENT,
+  CLEAR_CURRENT
+} from '../actions/types';
 
 const initialState = {
   logs: null,
@@ -15,24 +25,47 @@ export default (state = initialState, action) => {
         logs: action.payload,
         loading: false
       };
-      case ADD_LOG:
-        return {
-          ...state,
-          logs: [...state.logs, action.payload],
-          loading:false
-        }
+    case ADD_LOG:
+      return {
+        ...state,
+        logs: [...state.logs, action.payload],
+        loading: false
+      };
+    case DELETE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter(log => log.id !== action.payload),
+        loading: false
+      };
+    case UPDATE_LOG:
+      return {
+        ...state,
+        logs: state.logs.map(log =>
+          log.id === action.payload.id ? action.payload : log
+        )
+      };
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null
+      };
     case SET_LOADING:
       return {
         ...state,
         loading: true
       };
-      case DELETE_LOG:
-        return {
-          ...state,
-          logs:state.logs.filter(log => log.id !== action.payload),
-        }
     case LOGS_ERROR:
-      console.log(action.payload);
+      console.error(action.payload);
       return {
         ...state,
         error: action.payload
