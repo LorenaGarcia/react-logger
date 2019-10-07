16c6601d7e3213d93989b2d96ea01c5a0561cf3e
LorenaGarcia <lorraine@Lorenas-MacBook-Pro.local>
2019-10-06 21:35:33 -0500

    Updates

---
+++ precache-manifest.fb963484db6c36ffd4ab1f387da1b03a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0bb2c948d2cccdd06c27038ee8e78a",
+    "url": "/react-logger/index.html"
+  },
+  {
+    "revision": "4f86c8904f5a9a816c9f",
+    "url": "/react-logger/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "96bfc4524c1c30005fec",
+    "url": "/react-logger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "4f86c8904f5a9a816c9f",
+    "url": "/react-logger/static/js/2.a9d46d79.chunk.js"
+  },
+  {
+    "revision": "96bfc4524c1c30005fec",
+    "url": "/react-logger/static/js/main.0a228c85.chunk.js"
+  },
+  {
+    "revision": "64e5c5aadbc2655dea85",
+    "url": "/react-logger/static/js/runtime~main.2c73b55c.js"
+  }
+]);
